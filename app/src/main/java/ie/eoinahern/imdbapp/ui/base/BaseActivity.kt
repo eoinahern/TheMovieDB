31e46d2521a7f65c6a9ea3d03fe1692672031db5
eoinahern <eoin.ahern@mycit.ie>
2019-07-19 11:48:10 +0100

    added some funtionality to mainActivity

---
+++ app/src/main/java/ie/eoinahern/imdbapp/ui/base/BaseActivity.kt
@@ -0,0 +1,15 @@
+package ie.eoinahern.imdbapp.ui.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(getLayout())
+    }
+
+    abstract fun getLayout(): Int
+}
--- app/src/main/java/ie/eoinahern/imdbapp/ui/main/MainActivity.kt
+++ app/src/main/java/ie/eoinahern/imdbapp/ui/main/MainActivity.kt
@@ -1,29 +1,59 @@
 package ie.eoinahern.imdbapp.ui.main
 
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.android.AndroidInjection
-
 import ie.eoinahern.imdbapp.R
+import ie.eoinahern.imdbapp.data.model.MovieDetails
+import ie.eoinahern.imdbapp.ui.base.BaseActivity
+import ie.eoinahern.imdbapp.util.ErrorState
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     @Inject
-    lateinit var sharedPrefs: SharedPreferences
+    lateinit var adapter: MainActivityAdapter
 
+    class EmptyListState : ErrorState.CustomErrorState()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initAdapter()
+        initViewModel()
+    }
 
-        try {
-            val boo = sharedPrefs.getBoolean("boo", true)
-        } catch (e: Exception) {
-            println("no prefs !!!")
+    override fun getLayout(): Int {
+        return R.layout.activity_main
+    }
+
+    private fun initAdapter() {
+        recycler.adapter = adapter
+    }
+
+    private fun initViewModel() {
+
+    }
+
+    fun updateAdapter(list: List<MovieDetails>) {
+
+    }
+
+    fun displayError(error: ErrorState) {
+
+        when (error) {
+            is EmptyListState -> println("empty List")
+            is ErrorState.NetworkError -> println("error")
+            is ErrorState.IOError -> println("net error")
+            else -> println("error loading")
         }
+    }
+
+    fun showLoading() {
+
+    }
+
+    fun hideLoading() {
 
     }
 }
