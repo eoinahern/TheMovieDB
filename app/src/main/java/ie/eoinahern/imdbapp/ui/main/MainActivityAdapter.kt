a4276c01a87cab9c7f941196408b63ac8c30e07f
eoinahern <eoin.ahern@mycit.ie>
2019-07-20 12:41:40 +0100

    bind to view

--- app/src/main/java/ie/eoinahern/imdbapp/ui/main/MainActivityAdapter.kt
+++ app/src/main/java/ie/eoinahern/imdbapp/ui/main/MainActivityAdapter.kt
@@ -3,9 +3,15 @@ package ie.eoinahern.imdbapp.ui.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import ie.eoinahern.imdbapp.R
 import ie.eoinahern.imdbapp.data.model.MovieDetails
+import ie.eoinahern.imdbapp.util.IMAGE_URL
+import kotlinx.android.synthetic.main.movie_item_layout.view.*
+import okhttp3.HttpUrl
 import javax.inject.Inject
 
 class MainActivityAdapter @Inject constructor() : RecyclerView.Adapter<MainActivityAdapter.ViewHolder>() {
@@ -35,12 +41,22 @@ class MainActivityAdapter @Inject constructor() : RecyclerView.Adapter<MainActiv
 
     inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
 
+        private val image: ImageView by lazy { v.findViewById(R.id.movieImage) }
+        private val movieTitle: TextView by lazy { v.findViewById(R.id.movieTitle) }
+        private val movieYear: TextView by lazy { v.findViewById(R.id.movieYear) }
+
         init {
             itemView.setOnClickListener { listener(movieList[adapterPosition]) }
         }
 
         fun bind(movie: MovieDetails) {
+            Glide.with(image.context)
+                .load(HttpUrl.parse(IMAGE_URL.plus(movie.poster_path)))
+                .into(image)
 
+            movieTitle.text = movie.title
+            movieYear.text = movie.release_date
         }
+
     }
 }
