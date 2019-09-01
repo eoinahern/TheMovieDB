1008a5e550eebc9f307a4d8d26b95823bb788cf1
eoinahern <eoin.ahern@mycit.ie>
2019-09-01 14:23:50 +0100

    didnt need httpurl.parse call

--- app/src/main/java/ie/eoinahern/imdbapp/ui/main/MainActivityAdapter.kt
+++ app/src/main/java/ie/eoinahern/imdbapp/ui/main/MainActivityAdapter.kt
@@ -14,7 +14,8 @@ import kotlinx.android.synthetic.main.movie_item_layout.view.*
 import okhttp3.HttpUrl
 import javax.inject.Inject
 
-class MainActivityAdapter @Inject constructor() : RecyclerView.Adapter<MainActivityAdapter.ViewHolder>() {
+class MainActivityAdapter @Inject constructor() :
+    RecyclerView.Adapter<MainActivityAdapter.ViewHolder>() {
 
     private var movieList: MutableList<MovieDetails> = mutableListOf()
     var listener: (MovieDetails) -> Unit = { _ -> }
@@ -40,17 +41,20 @@ class MainActivityAdapter @Inject constructor() : RecyclerView.Adapter<MainActiv
     }
 
     inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        private val image: ImageView by lazy { v.findViewById(R.id.movieImage) }
-        private val movieTitle: TextView by lazy { v.findViewById(R.id.movieTitle) }
-        private val movieYear: TextView by lazy { v.findViewById(R.id.movieYear) }
+        private val image: ImageView by lazy { v.findViewById<ImageView>(R.id.movieImage) }
+        private val movieTitle: TextView by lazy { v.findViewById<TextView>(R.id.movieTitle) }
+        private val movieYear: TextView by lazy { v.findViewById<TextView>(R.id.movieYear) }
 
         init {
             itemView.setOnClickListener { listener(movieList[adapterPosition]) }
         }
 
         fun bind(movie: MovieDetails) {
+
+            val imageurl = IMAGE_URL.plus(movie.poster_path)
+
             Glide.with(image.context)
-                .load(HttpUrl.parse(IMAGE_URL.plus(movie.poster_path)))
+                .load(imageurl)
                 .into(image)
 
             movieTitle.text = movie.title
