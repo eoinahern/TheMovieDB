978a7ef3746306d9464e2694cf0d530f1f212b01
eoinahern <eoin.ahern@mycit.ie>
2019-09-10 20:16:52 +0100

    added back navigation

--- app/src/main/java/ie/eoinahern/imdbapp/ui/details/DetailsActivity.kt
+++ app/src/main/java/ie/eoinahern/imdbapp/ui/details/DetailsActivity.kt
@@ -2,15 +2,34 @@ package ie.eoinahern.imdbapp.ui.details
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import ie.eoinahern.imdbapp.R
+import ie.eoinahern.imdbapp.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_details.*
 
-class DetailsActivity : AppCompatActivity() {
+class DetailsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        toolbarSetup()
+    }
+
+    private fun toolbarSetup() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun getLayout(): Int = R.layout.activity_details
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
